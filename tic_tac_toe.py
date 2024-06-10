10b0f7948669f01a784bd4bda703a574df3970a2
Andrey Dyo <de_andrei@mail.ru>
2024-06-10 20:02:44 +0900

    add test

---
+++ tic_tac_toe.py
@@ -0,0 +1,18 @@
+from gameparts import Board
+
+
+def main():
+    # Создать игровое поле - объект класса Board.
+    game = Board()
+    # Отрисовать поле в терминале.
+    game.display()
+    # Разместить на поле символ по указанным координатам - сделать ход.
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    # Перерисовать поле с учётом сделанного хода.
+    game.display()
+    print(game.__str__())
+
+
+if __name__ == '__main__':
+    main()
